be35e3bd31063499f858c55fea97467c071c0798
Lyda <lyda.michopoulou@gmail.com>
2019-04-17 10:06:27 +0300

    changing app.js

--- js/app.js
+++ js/app.js
@@ -3,69 +3,72 @@ jQuery (init);
 // Same as: jQuery ( document ).ready(init);
 
 //Our Application logic goes here: 
-function init ( $ ){
+function init( $ ){
 
     let options = {
         url: "../data.json",
-        success: jsonHandleer
+        success: jsonHandler
     }
+    let entries;
 
-    function removeDups (names){
+    function removeDups(names) {
         let unique = {};
         names.forEach(function(i) {
-            if (!unique[i]){
-                unique [i]= true; 
-            }
-        })
+          if(!unique[i]) { unique[i] = true; }
+        });
         return Object.keys(unique);
     }
 
-    function createOptions( listOfCitites ){
-        // 1. Get datalist
-        let datalist = document.querySelector ( "#cities-list");
-        // 1.1 TEST!!
-        // 2. Loop over unique cities array
-        listOfCitites.map( addOption );
-        
+    function createOptions( listOfCities ){
+
+        let datalist = document.querySelector( "#cities-list" );
+        listOfCities.map( addOption );
+
         function addOption( city ){
-            datalist.innerHTML += `<option value="${city}"><option>`;
+            datalist.innerHTML += `<option value="${ city }"></option>`;
         }
-        // 2.1 Inside loop:
-            // Create option element
-            // Add city value
-            // Add to datalist (innerHTML)
-            // HINT: datalist.innerHTML = "<option>... </option>";  
+
     }
 
-    function jsonHandleer (data) {
-        
-        function getCity (hotel) {
+    function jsonHandler( data ){
+
+        function getCity( hotel ){ 
             return hotel.city;
         }
 
-        let entries = data[1].entries;
-        let cities = entries.map (getCity); //Array {Hotel} x 4
-        let uniqueCities = removeDups(cities);
+        entries = data[1].entries;
+        let cities = entries.map( getCity ); 
+        let uniqueCities = removeDups( cities ); 
         uniqueCities.sort();
-        
-        createOptions(uniqueCities);
-        
+        createOptions( uniqueCities );
 
     }
 
-    // HANDLE CITY INPUT
+    function getHotelsFromSelected( selectedCity ){
 
-    let citiesInput = document.querySelector("#cities");
-    citiesInput.addEventListener ("keydown", handleCityInput);
+        function filterHotels( hotel ){
+            return hotel.city.toLowerCase() === selectedCity.toLowerCase().trim();
+        }
 
-    function handleCityInput (e){
-        console.log ( e );
-        if (e.keycode == 13) {
-            console.log ("get hotels from city..");
+        let availableHotels = entries.filter( filterHotels );
+
+        return availableHotels;
+
+    }
+
+    function handleCityInput( e ){
+        if ( e.keyCode === 13 ){
+            let selectedCity = this.value;
+            getHotelsFromSelected( selectedCity );
         }
     }
 
-    $.ajax (options);
+    // HANDLE CITY INPUT 
+    let citiesInput = document.querySelector( "#cities" );
+    
+    citiesInput.addEventListener( "keydown", handleCityInput );
+
+    $.ajax( options );
 
 }
 
